56944784adc17590170a94f699d4d12ba599e1c3
DaryaBelomyzova <d.belomyzova@gmail.com>
2018-11-06 15:28:45 +0300

    Change the Days file

--- Lesson_2/Days/src/Days.java
+++ Lesson_2/Days/src/Days.java
@@ -9,35 +9,32 @@ public class Days {
             System.out.println("The day is: ");
 
 
-            if (number >= 1 && number <= 7) {
-                if (number <= 3) {
-                    if (number >= 2) {
-                        if (number == 2) {
-                            System.out.println("вторник");
-                        } else {
-                            System.out.println("среда");
-                        }
-                    } else {
-                        System.out.println("понедельник");
-                    }
-                } else {
-                    if (number <= 5) {
-                        if (number == 4) {
-                            System.out.println("четверг");
-                        } else {
-                            System.out.print("пятница");
-                        }
-                    } else {
-                        if (number == 6) {
-                            System.out.println("суббота");
-                        } else {
-                            System.out.println("воскресенье");
-                        }
-                    }
-                }
+            switch (number){
+                case 1:
+                    System.out.println("Monday");
+                    break;
+                case 2:
+                    System.out.println("Tuesday");
+                    break;
+                case 3:
+                    System.out.println("Wednesday");
+                    break;
+                case 4:
+                    System.out.println("Thursday");
+                    break;
+                case 5:
+                    System.out.println("Friday");
+                    break;
+                case 6:
+                    System.out.println("Saturday");
+                    break;
+                case 7:
+                    System.out.println("Sunday");
+                    break;
+                default:
+                    System.out.println("Unknown day");
+                    break;
 
-            } else {
-                System.out.println("такого дня недели не существует");
             }
 
         } while (true);
